116a71d8caa6206b6c34c71daf1fc2cfd5dc202b
Łukasz Krzyszczak <lukasz.krzyszczak@finder.com>
2023-02-03 13:52:25 +0100

    Add ECS

---
+++ ecs.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/bin',
+        __DIR__ . '/config',
+        __DIR__ . '/migrations',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::PSR_12,
+        SetList::STRICT,
+    ]);
+};
